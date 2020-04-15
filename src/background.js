ec772c563f6e5f2b9544189690e1ece27c80f4b4
Mihai Voinea <hi@voinea.dev>
2020-04-16 01:04:47 +0300

    Get auth code working on chrome & firefox

--- src/background.js
+++ src/background.js
@@ -1,36 +1,34 @@
 /* eslint-disable no-undef */
 import store from "./store";
 
-global.browser = require("webextension-polyfill");
+const browser = require("webextension-polyfill");
 
 const launchNewTab = () => {
-  browser.tabs.create({ url: "about:newtab" });
+  browser.tabs.create({});
 };
 
 browser.runtime.onInstalled.addListener(launchNewTab);
 
-browser.browserAction.onClicked.addListener(launchNewTab);
+browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  const { url } = changeInfo;
+  if (
+    url &&
+    url
+      .toLowerCase()
+      .startsWith(
+        "https://makerlog-homepage.netlify.app/.netlify/functions/callback?code="
+      )
+  ) {
+    // Old code to get the 'code' param
+    // const code = url.match(
+    // /(?<=(https\:\/\/makerlog\-homepage\.netlify\.app\/\.netlify\/functions\/callback\?code\=)).*$/i
+    // )[0];
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  const url = { changeInfo };
-  if (url) {
-    // Do something when the url changes
-    if (
-      url
-        .toLowerCase()
-        .startsWith(
-          "https://makerlog-homepage.netlify.app/.netlify/functions/callback?code="
-        )
-    ) {
-      /*
-      const code = url.match(
-        /(?<=(https\:\/\/makerlog-homepage\.netlify\.app\/\.netlify\/functions\/callback\?code\=)).*$/i
-      )[0];
-      */
-      const urlParams = new URLSearchParams(url);
-      const code = urlParams.get("code");
-      console.log(code);
-      if (code) store.dispatch("auth", { code });
-    }
+    // Convert the 'url' string to an URL object.
+    const urlObject = new URL(url);
+    // Get the 'code' param
+    const code = urlObject.searchParams.get("code");
+
+    console.log(code);
   }
 });
